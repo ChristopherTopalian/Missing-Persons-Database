ebf76673e88f8020d1df48d4570994019c388827
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2020-05-17 22:45:00 -0400

    Add files via upload

---
+++ _missing_persons.js
@@ -0,0 +1,333 @@
+records = [
+    
+    {
+    /*DEBORAH*/
+    name: "Debra Spickler",
+    age: "13",
+    dob: "1/22/1955",
+    dateMissing: "7/24/1968",
+    dayMissing: "Wednesday ", 
+    timeMissing: "?",   
+    locationMissing: "Henry Park, Vernon, CT, 06066",
+    locationMissingDetails: "Henry Park in the 100 block of South Street. Was visiting family in Vernon. They all went to Henry Park on 120 South Street, in Vernon. Last seen walking with a cousin from Foxhill Drive towards the parks swimming pool.", 
+    image: "Debra Spickler.PNG",
+    height: "5 feet, 5 inches",
+    weight: "125",
+    hairColor: "Brown",
+    eyeColor: "Brown",
+    suspects: "Suspect 1",
+    burialSite: "Body NOT Found",
+    urlLink: "http://charleyproject.org/case/deborah-lee-spickler",
+    urlMap: "https://www.google.com/maps/place/120+South+St,+Vernon,+CT+06066/@41.8571017,-72.4453417,175m/data=!3m1!1e3!4m5!3m4!1s0x89e6f68bf692b9f5:0x13c48fbf4d8879cf!8m2!3d41.856943!4d-72.4445877",
+    tag: "missing"
+    /*DEBORAH*/
+    },  
+        
+    {
+    /*IRENE*/
+    name: "Irene Lanora LaRosa",
+    age: "17Y, 10m, 20d",
+    dob: "4/10/1953",
+    dateMissing: "03/01/1971",
+    dayMissing: "Wednesday ",  
+    timeMissing: "?",  
+    locationMissing: "ELLINGTON, CT, 06029",
+    locationMissingDetails: "details here.",
+    image: "Irene Larosa.png",
+    height: "5 feet, 3 inches",
+    weight: "110",
+    hairColor: "Brown",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    urlMap: "https://www.google.com/maps/place/Ellington,+CT+06029/@41.9084769,-72.5635453,11z/data=!3m1!4b1!4m5!3m4!1s0x89e6f11445301381:0xfe3e6537da80364a!8m2!3d41.9223813!4d-72.4613191",
+    burialSite: "Body NOT Found",
+    urlLink: "http://charleyproject.org/case/irene-lanora-larosa",
+    tag: "missing"
+    /*IRENE*/
+    },
+        
+    {
+    /*JANICE*/
+    name: "Janice Kathryn Pockett",
+    age: "7",
+    dob: "10/15/1965",
+    dateMissing: "7/26/1973",
+    dayMissing: "Thursday",  
+    timeMissing: "?",  
+    locationMissing: "Rhodes Rd, Tolland, CT, 06084",
+    locationMissingDetails: "She left for a bike ride but never returned home on Anthony Road in Tolland",
+    image: "janice pockett.png",
+    height: "4 feet, 0 inches",
+    weight: "65",
+    hairColor: "Blonde",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    urlMap: "https://www.google.com/maps/place/Rhodes+Rd,+Tolland,+CT+06084/@41.861024,-72.3448869,17z/data=!3m1!4b1!4m5!3m4!1s0x89e6f4b0ab301cef:0x990b205e7f7a82bc!8m2!3d41.861024!4d-72.3426982",
+    burialSite: "Body NOT Found. Her bike was found nearby Rhodes Road next to a wooded area.",
+    urlLink: "http://charleyproject.org/case/janice-kathryn-pockett",
+    tag: "missing"  
+    /*JANICE*/
+    },
+      
+    {
+    /*KATHLEEN*/
+    name: "Kathleen Lee Terry",
+    age: "12",
+    dob: "11/15/1961",
+    dateMissing: "7/19/1974",
+    dayMissing: "Friday", 
+    timeMissing: "?",   
+    locationMissing: "?",
+    locationMissingDetails: "Kathy was murdered at age 12 while riding her bicycle down a wooded road in Sturbridge, Massachusetts.",
+    image: "Kathleen Terry.png",
+    height: "?",
+    weight: "?",
+    hairColor: "?",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    burialSite: "Burial Site. Body found next morning, 7/20/1974, Saturday, around 9am, off of Rte. 15, about a mile inside the MA border, which is about 20 miles northeast of where Janice was last seen. Died from a heavy blow to the head, possibly from a rock. NO Sign of Sexual Assault. ",
+    urlLink: "https://www.findagrave.com/memorial/6919492/kathleen-terry",
+    tag: "missing"
+    /*KATHLEEN*/
+    },
+      
+    {
+    /*LISA*/ 
+    name: "Lisa Joy White",
+    age: "13",
+    dob: "2/2/1961",
+    dateMissing: "11/1/1974",
+    dayMissing: "Friday", 
+    timeMissing: "?",   
+    locationMissing: "Vernon, Connecticut, 06066",
+    locationMissingDetails: "Last seen walking out of a park on Prospect Street into downtown Rockville on the night of 11/1/1974. She had visited a friend and lived just two miles from Prospect. ",
+    image: "Lisa White.png",
+    height: "5 feet, 0 inches",
+    weight: "110",
+    hairColor: "Blonde",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    burialSite: "Burial Site - Body NOT Found",
+    urlLink: "http://charleyproject.org/case/lisa-joy-white",
+    tag: "missing"  
+    },
+        
+    {
+    /*SUSAN*/
+    name: "Susan LaRosa",
+    age: "20Y, 11m",
+    dob: "7/22/1954",
+    dateMissing: "6/22/1975",
+    dayMissing: "Sunday",
+    timeMissing: "?",    
+    locationMissing: "Vernon, Connecticut. Walking toward Rockville, Connecticut",
+    locationMissingDetails: "HER SISTER IN LAW, IRENE LaRosa is ALSO a VICTIM of Abduction, in 1971",
+    image: "Susan Larosa2.png",
+    height: "?",
+    weight: "?",
+    hairColor: "?",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    burialSite: "Skeleton Remains Only, FOUND IN WOODS. SKULL WAS FRACTURED, possibly by bullet",
+    urlLink: "https://www.courant.com/news/connecticut/hc-xpm-2002-11-02-hc-cc-larosa-120202-story.html",
+    tag: "missing" 
+    /*SUSAN*/
+    },
+        
+    {
+    /*STEPHANIE*/
+    name: "Stephanie Olisky",
+    age: "15",
+    dob: "UNKNOWN",
+    dateMissing: "7/21/1975",
+    dayMissing: "Monday",
+    timeMissing: "?",    
+    locationMissing: "East Windsor, Connecticut",
+    locationMissingDetails: "FOUND Dying with Head Injuries on Rt. 5, East Windsor, CT. She died days later from complications of serious head trauma. Happened 2 towns away from where Lisa White disappeared. ",
+    image: "Stephanie Olisky2.png",
+    height: "?",
+    weight: "?",
+    hairColor: "?",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    burialSite: "DIED FROM HEAD TRAUMA",
+    urlLink: "https://www.courant.com/news/connecticut/hc-xpm-1998-07-21-9807210344-story.html",
+    tag: "missing"  
+    /*STEPHANIE*/
+    },
+         
+    {
+    /*DAWN*/
+    name: "Dawn M. Peterson",
+    age: "13",
+    dob: "10/13/1962 ",
+    dateMissing: "3/26/1976",
+    dayMissing: "Friday",
+    timeMissing: "?",    
+    locationMissing: "North Windham, CT",
+    locationMissingDetails: "Which is a Few Towns Away from Vernon and Tolland",
+    image: "Dawn Peterson.png",
+    height: "?",
+    weight: "?",
+    hairColor: "?",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    burialSite: "Burial Site ?",
+    urlLink: "https://savagewatch.com/connecticut-cases/",
+    tag: "missing"   
+    /*DAWN*/     
+    },
+    
+    {
+    /*PATRICIA*/
+    name: "Patricia E. Luce",
+    age: "18",
+    dob: "?",
+    dateMissing: "7/18/1978",
+    dayMissing: "Tuesday",  
+    timeMissing: "?",  
+    locationMissing: "7-Eleven convenience store on Route 83, not far from her family's Rockville home.",
+    locationMissingDetails: "?",
+    image: "Patricia Luce.png",
+    height: "?",
+    weight: "?",
+    hairColor: "?",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    burialSite: "BURIAL SITE: Luce's skeletal remains were found by two people walking in the woods in Marlborough March 13, 1979, and the state's chief medical examiner determined she was slain by blunt force that caused multiple skull fractures. FOUND IN WOODS",
+    urlLink: "https://www.courant.com/hc-cc-luce-032907-story.html",
+    tag: "missing"
+    /*PATRICIA*/
+    },
+           
+    {
+    /*HOLLY*/
+    name: "Holly Piirainen",
+    age: "10",
+    dob: "1/19/1983",
+    dateMissing: "8/5/1993",
+    dayMissing: "Thursday",  
+    timeMissing: "?",  
+    locationMissing: "Sturbridge, MA",
+    locationMissingDetails: "?",
+    image: "Holly Piirainen.png",
+    height: "4 feet, 6 inches",
+    weight: "?",
+    hairColor: "Blonde",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    burialSite: "On October 23, 1993, Piirainen's remains were found by hunters in Brimfield, Massachusetts. STRANGULATION",
+    urlLink: "https://en.wikipedia.org/wiki/Holly_Piirainen",
+    tag: "missing"
+    /*HOLLY*/
+    },
+   
+    {
+    /*MOLLY*/  
+    name: "Molly Bish",
+    age: "16Y, 10m, 25d",
+    dob: "8/2/1983",
+    dateMissing: "6/27/2000",
+    dayMissing: "Tuesday", 
+    timeMissing: "?",   
+    locationMissing: "Comins Pond, Warren, MA",
+    locationMissingDetails: "St. Pauls Cemetery Trail FROM Comins Pond Beach",
+    image: "Molly Bish.png",
+    height: "5 feet, 7 inches",
+    weight: "125",
+    hairColor: "Blonde",
+    eyeColor: "Blue",
+    suspects: "Suspect 1",
+    burialSite: "20+ BONES FOUND, Skeleton Remains Only, FOUND IN WOODS",
+    urlLink: "https://en.wikipedia.org/wiki/Death_of_Molly_Bish",
+    tag: "missing" 
+    /*MOLLY*/ 
+    },
+        
+    {
+    /*TAMMY*/
+    name: "Tammy Sandoval",
+    age: "17",
+    dob: "11/21/2001 hispanic",
+    dateMissing: "6/26/2019",
+    dayMissing: "Thursday", 
+    timeMissing: "?",   
+    locationMissing: "Enfield, Connecticut",
+    locationMissingDetails: "test",
+    image: "Tammy Sandoval.png",
+    height: "5 feet, 0 inches",
+    weight: "145",
+    hairColor: "BLACK",
+    eyeColor: "BROWN",
+    suspects: "Suspect 1",
+    burialSite: "POSSIBLE RUNAWAY",
+    urlLink: "https://www.missingalert.com/Missing/Connecticut/TAMMY_SANDOVAL",
+    tag: "missing"
+    /*TAMMY*/
+    },
+        
+    {
+    /*ANGELICA*/
+    name: "Angelica Martinez",
+    age: "23",
+    dob: "?",
+    dateMissing: "April 20, 2003 date found",
+    dayMissing: "?",  
+    timeMissing: "?",  
+    locationMissing: "240 Chestnut St, in Springfield MA.",
+    locationMissingDetails: "behind Club X-Static",
+    image: "",
+    height: "?",
+    weight: "?",
+    hairColor: "?",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    burialSite: "?",
+    urlLink:"?",
+    tag: "missing"
+    /*ANGELICA*/
+    },
+        
+    /*AWILDA*/
+    {
+    name: "Awilda Nieves",
+    age: "29",
+    dob: "?",
+    dateMissing: "June 19, 2001 date found dead",
+    dayMissing: "?",
+    timeMissing: "?",    
+    locationMissing: "near Anniversary Road in Holyoke, MA.",
+    locationMissingDetails: "behind the Super 8 Motel",
+    image: "?",
+    height: "?",
+    weight: "?",
+    hairColor: "?",
+    eyeColor: "?",
+    suspects: "Suspect 1",
+    burialSite: "?",
+    urlLink:"?",
+    tag: "missing"    
+    /*AWILDA*/  
+    }
+
+    ];
+    
+    
+    /*
+    name: "test",
+    age: "test",
+    dob: "test",
+    dateMissing: "test",
+    dayMissing: "test", 
+    timeMissing: "test",   
+    locationMissing: "test",
+    locationMissingDetails: "test",
+    image: "test",
+    height: "test",
+    weight: "test",
+    hairColor: "test",
+    eyeColor: "test",
+    suspects: "Suspect 1",
+    burialSite: "test",
+    tag: "missing"
+    */
